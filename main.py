fb079ae495427af6480b4233d64444cf564d115c
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-05-30 12:13:05 -0300

    Change the name of the utils functions v2 to predictor functions

--- main.py
+++ main.py
@@ -1,45 +1,47 @@
-import utils.helper_functions_v2 as hf
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+import utils.predictor_functions as pf
 
 league = 'premier-league'
 seasons = '2017-2023'
 season_test = 2022
 
 # Read the data
-X_full, y, X_test_full, y_test, odds_test = hf.get_league_data(league, seasons, season_test)
+X_full, y, X_test_full, y_test, odds_test = pf.get_league_data(league, seasons, season_test)
 
 # Define categorical and numerical columns
-categorical_cols, numerical_cols = hf.set_numerical_categorical_cols(X_full)
+categorical_cols, numerical_cols = pf.set_numerical_categorical_cols(X_full)
 
 # Keep selected columns only
-X_train, y_train, X_test = hf.filter_datasets(X_full, y, X_test_full, categorical_cols, numerical_cols)
+X_train, y_train, X_test = pf.filter_datasets(X_full, y, X_test_full, categorical_cols, numerical_cols)
 
 # Transform numerical and categorical cols
-X_train = hf.transform_x(X_train, categorical_cols, numerical_cols)
-X_test = hf.transform_x(X_test, categorical_cols, numerical_cols)
+X_train = pf.transform_x(X_train, categorical_cols, numerical_cols)
+X_test = pf.transform_x(X_test, categorical_cols, numerical_cols)
 
 # Get mi scores
-first_mi_scores = hf.make_mi_scores(X_train, y_train)
+first_mi_scores = pf.make_mi_scores(X_train, y_train)
 
 # Create cluster features
-hf.create_cluster_features(X_train, X_test, first_mi_scores)
+pf.create_cluster_features(X_train, X_test, first_mi_scores)
 
 # Get mi scores including the cluster features
-second_mi_scores = hf.make_mi_scores(X_train, y_train)
+second_mi_scores = pf.make_mi_scores(X_train, y_train)
 
 # Create PCA features
-X_train, X_test = hf.apply_pca_datasets(X_train, X_test, second_mi_scores)
+X_train, X_test = pf.apply_pca_datasets(X_train, X_test, second_mi_scores)
 
 # Get mi scores including the PCA features
-third_mi_scores = hf.make_mi_scores(X_train, y_train)
+third_mi_scores = pf.make_mi_scores(X_train, y_train)
 
 # Get random forest model via random search
-from sklearn.ensemble import RandomForestClassifier
-model = RandomForestClassifier(criterion='entropy', max_depth=50, max_features='log2',
-                         min_samples_leaf=2, min_samples_split=10,
-                         n_estimators=2000, random_state=0)
+model = LogisticRegression(random_state=0, penalty='l2', C=0.1)
+# model = RandomForestClassifier(criterion='entropy', max_depth=None, max_features='log2',
+#                          min_samples_leaf=2, min_samples_split=25,
+#                          n_estimators=500, random_state=0)
 
 # Build pipeline
-my_pipeline = hf.build_pipeline(X_train, y_train, model)
+my_pipeline = pf.build_pipeline(X_train, y_train, model)
 
 # Generate statistics
-test_results_df = hf.build_pred_df(my_pipeline, X_test, y_test, odds_test)
+test_results_df = pf.build_pred_df(my_pipeline, X_test, y_test, odds_test)
--- model_tester.py
+++ model_tester.py
@@ -1,35 +1,35 @@
-import utils.helper_functions_v2 as hf
+import utils.predictor_functions as pf
 
-league = 'premier-league'
-seasons = '2017-2023'
-season_start = 2018
+league = 'mls'
+seasons = '2018-2024'
+season_start = 2019
 season_end = 2023
 
 for season in range(season_start, season_end):
     # Read the data
-    X_full, y, X_test_full, y_test, odds_test = hf.get_league_data(league, seasons, season)
+    X_full, y, X_test_full, y_test, odds_test = pf.get_league_data(league, seasons, season)
 
     # Define categorical and numerical columns
-    categorical_cols, numerical_cols = hf.set_numerical_categorical_cols(X_full)
+    categorical_cols, numerical_cols = pf.set_numerical_categorical_cols(X_full)
 
     # Keep selected columns only
-    X_train, y_train, X_test = hf.filter_datasets(X_full, y, X_test_full, categorical_cols, numerical_cols)
+    X_train, y_train, X_test = pf.filter_datasets(X_full, y, X_test_full, categorical_cols, numerical_cols)
 
     # Transform numerical and categorical cols
-    X_train = hf.transform_x(X_train, categorical_cols, numerical_cols)
-    X_test = hf.transform_x(X_test, categorical_cols, numerical_cols)
+    X_train = pf.transform_x(X_train, categorical_cols, numerical_cols)
+    X_test = pf.transform_x(X_test, categorical_cols, numerical_cols)
 
     # Get mi scores
-    first_mi_scores = hf.make_mi_scores(X_train, y_train)
+    first_mi_scores = pf.make_mi_scores(X_train, y_train)
 
     # Create cluster features
-    hf.create_cluster_features(X_train, X_test, first_mi_scores)
+    pf.create_cluster_features(X_train, X_test, first_mi_scores)
 
     # Get mi scores including the cluster features
-    second_mi_scores = hf.make_mi_scores(X_train, y_train)
+    second_mi_scores = pf.make_mi_scores(X_train, y_train)
 
     # Create PCA features
-    X_train, X_test = hf.apply_pca_datasets(X_train, X_test, second_mi_scores)
+    X_train, X_test = pf.apply_pca_datasets(X_train, X_test, second_mi_scores)
 
     # Run the random search algorithm for a few machine learning models
-    hf.run_random_search(X_train, y_train, season, league)
+    pf.run_random_search(X_train, y_train, season, league)
