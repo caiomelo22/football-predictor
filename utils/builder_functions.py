13503c62d2a4ff9f122ada8e078caf449c0f6cef
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-06-21 11:52:34 -0300

    Making the csv_builder algorithm more generic to work with more numerical variables

--- utils/builder_functions.py
+++ utils/builder_functions.py
@@ -13,17 +13,30 @@ def get_winner(home_score, away_score):
     
 def get_games_results(games, scenario):
     loser = 'A' if scenario == 'H' else 'H'
-    return len(games.loc[games['winner'] == scenario].index), len(games.loc[games['winner'] == 'D'].index), len(games.loc[games['winner'] == loser].index)
+    return len(games.loc[games['winner'] == scenario]), len(games.loc[games['winner'] == 'D']), len(games.loc[games['winner'] == loser])
 
 def get_stats_mean(games, n_last_games, scenario):
     games = games.iloc[-n_last_games:,:]
     
-    team_stats = [games['team_score'].mean(), games['opp_score'].mean(), games['team_xg'].mean(), games['opp_xg'].mean()]
+    prefixes = ['team_', 'opp_']
+    filtered_cols = [
+        col for col in games.columns 
+        if any(col.startswith(prefix) for prefix in prefixes) and games[col].dtype in (float, 'int32', 'int64')
+    ]
+    team_stats_dict = games[filtered_cols].mean().to_dict()
     
-    return team_stats
+    return team_stats_dict
 
-def get_historical_stats(games, home_games, away_games):
-    total_games = len(games.index)
+def drop_total_games_keys(historical_dict):
+    key_to_drop = [
+        'home_wins', 'home_draws', 'home_losses',
+        'away_wins', 'away_draws', 'away_losses'
+    ]
+    historical_dict = {key: value for key, value in historical_dict.items() if key not in key_to_drop}
+    return historical_dict
+
+def get_historical_stats(games, home_games, away_games, suffix=''):
+    total_games = len(games)
     home_wins, home_draws, home_losses = get_games_results(home_games, 'H')
     away_wins, away_draws, away_losses = get_games_results(away_games, 'A')
     
@@ -31,14 +44,25 @@ def get_historical_stats(games, home_games, away_games):
     total_draws = home_draws + away_draws
     total_losses = home_losses + away_losses
     
-    win_pct = total_wins * 100 / total_games
-    draw_pct = total_draws * 100 / total_games
-    loss_pct = total_losses * 100 / total_games
+    win_pct = total_wins / total_games
+    draw_pct = total_draws / total_games
+    loss_pct = total_losses / total_games
     
     points_achieved = total_wins * 3 + total_draws
-    points_pct = (points_achieved * 100) / (total_games * 3)
+    points_pct = points_achieved / (total_games * 3)
     
-    return points_pct, win_pct, draw_pct, loss_pct, home_wins, home_draws, home_losses, away_wins, away_draws, away_losses
+    historical_keys = [
+        'points_pct', 'win_pct', 'draw_pct', 'loss_pct',
+        'home_wins', 'home_draws', 'home_losses',
+        'away_wins', 'away_draws', 'away_losses'
+    ]
+    historical_vals = [points_pct, win_pct, draw_pct, loss_pct, home_wins, home_draws, home_losses, away_wins, away_draws, away_losses]
+    
+    historical_dict = dict(zip(historical_keys, historical_vals))
+    if suffix: 
+        historical_dict = {f"{key}_{suffix}": value for key, value in historical_dict.items()}
+    
+    return dict(zip(historical_keys, historical_vals))
 
 def get_team_previous_games(team, game_date, season, fixtures_df):
     home_previous_games = fixtures_df.loc[(fixtures_df['home_team'] == team) & (fixtures_df['date'] < game_date)]
@@ -47,20 +71,13 @@ def get_team_previous_games(team, game_date, season, fixtures_df):
     if len(home_previous_games.index) == 0 or len(away_previous_games.index) == 0:
         return None
     
-    home_previous_games.rename(columns = {
-        'home_team': 'team', 'home_score': 'team_score', 'home_xg': 'team_xg',
-                                          
-        'away_team': 'opp', 'away_score': 'opp_score', 'away_xg': 'opp_xg',
-                                          
-        'home_odds': 'team_odds', 'away_odds': 'opp_odds'}, inplace=True)
+    home_games_cols_renamed = {col: col.replace('home_', 'team_').replace('away_', 'opp_') for col in home_previous_games.columns}
+    away_games_cols_renamed = {col: col.replace('away_', 'team_').replace('home_', 'opp_') for col in away_previous_games.columns}
+    
+    home_previous_games.rename(columns=home_games_cols_renamed, inplace=True)
     home_previous_games['scenario'] = 'H'
     
-    away_previous_games.rename(columns = {
-        'away_team': 'team', 'away_score': 'team_score', 'away_xg': 'team_xg',
-                                          
-        'home_team': 'opp', 'home_score': 'opp_score', 'home_xg': 'opp_xg',
-                                          
-        'away_odds': 'team_odds', 'home_odds': 'opp_odds'}, inplace=True)
+    away_previous_games.rename(columns=away_games_cols_renamed, inplace=True)
     away_previous_games['scenario'] = 'A'
     
     previous_games = pd.concat([home_previous_games, away_previous_games], axis=0, ignore_index=True)
@@ -82,33 +99,34 @@ def get_team_previous_games_stats(team, season, game_date, scenario, n_last_game
     if total_games < 10 or (len(home_previous_season_games.index) < 5 and scenario == 'H') or (len(away_previous_season_games.index) < 5 and scenario == 'A'):
         return
     
-    points_pct, win_pct, draw_pct, loss_pct, home_wins, home_draws, home_losses, away_wins, away_draws, away_losses = get_historical_stats(previous_season_games, home_previous_season_games, away_previous_season_games)
+    games_dict = get_historical_stats(previous_season_games, home_previous_season_games, away_previous_season_games)
     
     previous_last_games = previous_season_games.iloc[-n_last_games:,:]
     home_last_games = previous_last_games.loc[previous_last_games['scenario'] == 'H']
     away_last_games = previous_last_games.loc[previous_last_games['scenario'] == 'A']
     
-    points_pct_last_games, win_pct_last_games, draw_pct_last_games, loss_pct_last_games, home_wins_last_games, home_draws_last_games, home_losses_last_games, away_wins_last_games, away_draws_last_games, away_losses_last_games = get_historical_stats(previous_last_games, home_last_games, away_last_games)
+    last_games_dict = get_historical_stats(previous_last_games, home_last_games, away_last_games, suffix='last_games')
+    last_games_dict = drop_total_games_keys(last_games_dict)
     
     if scenario == 'H':
-        ha_win_pct = home_wins * 100 / len(home_previous_season_games.index)
-        ha_draw_pct = home_draws * 100 / len(home_previous_season_games.index)
-        ha_loss_pct = home_losses * 100 / len(home_previous_season_games.index)
+        prefix = 'home'
+        previous_season_games_filtered = home_previous_season_games
     else:
-        ha_win_pct = away_wins * 100 / len(away_previous_season_games.index)
-        ha_draw_pct = away_draws * 100 / len(away_previous_season_games.index)
-        ha_loss_pct = away_losses * 100 / len(away_previous_season_games.index)
-        
-    game_stats = get_stats_mean(previous_season_games, n_last_games, scenario)
-    
-    if any([np.isnan(s) for s in game_stats]): return None
-    
-    return [points_pct, win_pct, draw_pct, loss_pct, ha_win_pct, ha_draw_pct, ha_loss_pct, win_pct_last_games, draw_pct_last_games, loss_pct_last_games] + game_stats
+        prefix = 'away'
+        previous_season_games_filtered = away_previous_season_games
 
-def build_formatted_csv(data_model):
-    columns = ['game_date', 'season', 'home_team', 'away_team', 'home_odds', 'away_odds', 'draw_odds', 
-           'home_pts_pct', 'home_win_pct', 'home_draw_pct', 'home_loss_pct', 'home_home_win_pct', 'home_home_draw_pct', 'home_home_loss_pct', 'home_win_pct_last_games', 'home_draw_pct_last_games', 'home_loss_pct_last_games', 'home_score_last_games', 'home_conceded_last_games', 'home_xg_last_games', 'home_conceded_xg_last_games', 
-           'away_pts_pct', 'away_win_pct', 'away_draw_pct', 'away_loss_pct', 'away_away_win_pct', 'away_away_draw_pct', 'away_away_loss_pct', 'away_win_pct_last_games', 'away_draw_pct_last_games', 'away_loss_pct_last_games', 'away_score_last_games', 'away_conceded_last_games', 'away_xg_last_games', 'away_conceded_xg_last_games', 
-           'outcome', 'home_score', 'away_score']
-    data_df = pd.DataFrame(data_model, columns=columns)
-    return data_df
+    outcome_pct_dict = {
+        f'{prefix}_win_pct': games_dict[f'{prefix}_wins'] / len(previous_season_games_filtered),
+        f'{prefix}_draw_pct': games_dict[f'{prefix}_draws'] / len(previous_season_games_filtered),
+        f'{prefix}_loss_pct': games_dict[f'{prefix}_losses'] / len(previous_season_games_filtered),
+    }
+    games_dict = drop_total_games_keys(games_dict)
+        
+    team_stats_dict = get_stats_mean(previous_season_games, n_last_games, scenario)
+    
+    if any(np.isnan(value) for value in team_stats_dict.values()): return None
+    
+    return_dict = {**games_dict, **last_games_dict, **outcome_pct_dict, **team_stats_dict}
+    return_dict = {f"{prefix}_{key}": value for key, value in return_dict.items() if not key.startswith(prefix)}
+    
+    return return_dict
