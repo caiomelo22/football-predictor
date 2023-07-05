34c50649e919c245a4ba2d26305a4af93eb92fb3
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-07-05 10:11:29 -0300

    Message if there are no bets in the day

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from utils.leagues_info import leagues
 import json
 import os
 
-league = 'serie-a'
+league = 'major-league-soccer'
 league_info = leagues[league]
 n_last_games = 5
-bankroll = 474
+bankroll = 451
 
 options_path = f"leagues/{league}/official/columns.json"
 with open(options_path, 'r') as json_file:
@@ -83,14 +83,19 @@ preds_test_df = pd.DataFrame(predictions, index=data_df.index, columns=['pred'])
 test_results_df = pd.concat([preds_test_df, probs_test_df, next_games], axis=1)
 test_results_df = test_results_df.astype({'home_odds': float, 'draw_odds': float, 'away_odds': float})
 
+today_bets = 0
 for _, game in test_results_df.iterrows():
     bet_value = pf.get_bet_value_by_row(game, bankroll, options_info['strategy'])
     odds, probs = pf.get_bet_odds_probs(game)
     bet_worth_it = pf.bet_worth_it(bet_value, odds)
     if not bet_worth_it: continue
+    today_bets += 1
 
     print(f"\n{game['home_team']} ({game['home_odds']})")
     print(f"X ({game['draw_odds']})")
     print(f"{game['away_team']} ({game['away_odds']})")
     print(f"Prediction: {game['pred']} ({odds})")
     print(f"Bet Value: {bet_value}")
+
+if not today_bets:
+    print("\nSorry, there are no bets for today.")
